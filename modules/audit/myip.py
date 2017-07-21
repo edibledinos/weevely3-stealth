c93f82d3e293adcb53f361137bbe0d2183be074b
Sander Ferdinand <sa.ferdinand@gmail.com>
2017-07-21 20:39:42 +0200

    Added module linx-exploit suggest

--- modules/audit/myip.py
+++ modules/audit/myip.py
@@ -17,11 +17,6 @@ class Myip(Module):
             }
         )
 
-        # self.register_arguments([
-        #     {'name': '-real', 'help': 'Filter only real users', 'action': 'store_true', 'default': False},
-        #     {'name': '-vector', 'choices': ('posix_getpwuid', 'file', 'fread', 'file_get_contents', 'base64')}
-        # ])
-
     def run(self):
         result = PhpCode("""print $_SERVER['REMOTE_ADDR'];""").run()
         result = "$_SERVER['REMOTE_ADDR']: " + result
